fb84eae78e5a3b68a0945d8129edf5a5cf889645
nikos bakogiannis <nbakojohn13@gmail.com>
2024-08-29 11:20:34 +0200

    addign searchbar on index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,11 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :destroy]
 
   def index
-    @books = Book.all
+    if params[:query].present?
+      @books = Book.search_by_title_author_and_price(params[:query])
+    else
+      @books = Book.all
+    end
   end
 
   def show
